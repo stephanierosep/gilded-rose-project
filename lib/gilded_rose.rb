e6f050b90b94761de8d042852770394d6ad83863
Kay Lovelace <kay@makersacademy.com>
2018-10-07 16:19:01 +0100

    Extract Item class to file

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -52,19 +52,3 @@ class GildedRose
     end
   end
 end
-
-# Don't change anything below this line
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s
-    "#{@name}, #{@sell_in}, #{@quality}"
-  end
-end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,4 +1,5 @@
 require 'gilded_rose'
+require 'item'
 
 describe GildedRose do
 
